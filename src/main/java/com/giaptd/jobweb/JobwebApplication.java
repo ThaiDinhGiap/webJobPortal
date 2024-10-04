372b02a4fadf236c676e751bdb7a1267af33f23e
CoderFPT <139433806+CoderFPT@users.noreply.github.com>
2024-10-05 00:54:30 +0700

    Initial Version

---
+++ src/main/java/com/giaptd/jobweb/JobwebApplication.java
@@ -0,0 +1,13 @@
+package com.giaptd.jobweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobwebApplication.class, args);
+	}
+
+}
